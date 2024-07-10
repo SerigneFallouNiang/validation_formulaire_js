30cb3b6a52b8ba9d1859da1fc6149291e4872400
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-10 11:54:41 +0000

    validation d'email

--- valid.js
+++ valid.js
@@ -6,3 +6,30 @@
 // let prenom = "gael";
 // let regExp = new RegExp('a');
 // console.log(regExp.test(prenom));
+
+let form = document.querySelector('#loginForm');
+
+// console.log(form.email);
+// Ecouter la modification de l'email
+form.email.addEventListener('change',function(){
+validEmail(this);  
+});
+
+const validEmail = function (inputEmail){
+    // création de l'expression reguliaire pour validation email
+    let emailRegExp = new RegExp(
+'^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$',
+'g'
+    );
+
+let testEmail = emailRegExp.test(inputEmail.value);
+
+// nextElementSibling c'est pour attraper la balise qui est juste aprés
+let small = inputEmail.nextElementSibling;
+
+if(testEmail){
+    small.innerHTML = 'Adresse Valide';
+} else{
+    small.innerHTML = 'Adresse Non Valide';
+}
+};
