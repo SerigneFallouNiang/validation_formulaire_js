1eec5534bfc256e19d635168aa983e047e955856
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-10 11:16:16 +0000

    réalisation du formulaire en html et css

---
+++ valid.js
@@ -0,0 +1,8 @@
+// let prenom = "gael";
+// pour chercher si a se trouve dans notre variable prenom ce qui nous retour true
+// console.log(/b/.test(prenom));
+
+// oubien
+// let prenom = "gael";
+// let regExp = new RegExp('a');
+// console.log(regExp.test(prenom));
